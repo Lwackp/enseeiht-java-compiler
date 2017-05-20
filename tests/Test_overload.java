045a5a6d7daa978a9e226112d60c74a116e97da6
Lena David <davidl@bde.enseeiht.fr>
2017-05-20 16:32:26 +0200

    Add test for overload

---
+++ tests/Test_overload.java
@@ -0,0 +1,82 @@
+class Triplet {
+    private int x;
+    private int y;
+    private int z;
+
+    public Triplet(int _x, int _y, int _z) {
+        x = _x;
+        y = _y;
+        z = _z;
+    }
+
+    public Triplet(int _x, int _y) {
+        x = _x;
+        y = _y;
+        z = 0;
+    }
+
+    public Triplet(int _x) {
+        x = _x;
+        y = 0;
+        z = 0;
+    }
+
+    public Triplet() {
+        x = 0;
+        y = 0;
+        z = 0;
+    }
+
+    public int getFirst() {
+        return x;
+    }
+    
+    public int getSecond() {
+        return y;
+    }
+    
+    public int getThird() {
+        return z;
+    }
+
+    public void set(int _x) {
+        x = _x;
+    }
+
+    public void set(int _x, int _y) {
+        x = _x;
+        y = _y;
+    }
+
+    public void set(int _x, int _y, int _z) {
+        x = _x;
+        y = _y;
+        z = _z;
+    }
+}
+
+public class Test_multiple_constructors {
+    public static void main(String[] args) {
+        Triplet _triplet3 = new Triplet(1, 2, 3);
+        println _triplet3.getFirst();
+        println _triplet3.getSecond();
+        println _triplet3.getThird();
+        Triplet _triplet2 = new Triplet(4, 5);
+        println _triplet2.getFirst();
+        println _triplet2.getSecond();
+        println _triplet2.getThird();
+        
+        println "======================\n";
+
+        _triplet3.set(9, 8);
+        println _triplet3.getFirst();
+        println _triplet3.getSecond();
+        println _triplet3.getThird();
+
+        _triplet2.set(15, 13, 17);
+        println _triplet2.getFirst();
+        println _triplet2.getSecond();
+        println _triplet2.getThird();
+
+    }
+}
